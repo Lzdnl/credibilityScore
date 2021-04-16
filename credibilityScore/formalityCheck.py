bac4c70757713cfc0d94b79f8085fa55ba255077
Lzdnl <danilaeliza@gmail.com>
2021-04-16 17:04:18 +0200

    dict with results for formality check is now returned by the function

--- credibilityScore/formalityCheck.py
+++ credibilityScore/formalityCheck.py
@@ -1,6 +1,6 @@
 from autocorrect import Speller
 import re
-import os
+
 
 def formality_check():
 
@@ -8,10 +8,13 @@ def formality_check():
     marks_count = 0
     consecutive_marks_count = 0
     all_caps_words_count = 0
+    capitalized_words = []
     spelling_mistakes_count = 0
+    incorrect_words = []
     title_spelling_mistakes_count = 0
     unique_words = set([])
     total_word_count = 0
+    formality_results = {}
 
     spell = Speller(lang='en')
 
@@ -48,11 +51,11 @@ def formality_check():
                 continue
             if words[i].isupper():
                 if words[i+1].isupper() or words[i-1].isupper():
+                    capitalized_words.append(words[i])
                     all_caps_words_count += 1
                     # print('Found word in all caps sequence: ', words[i])
 
         # Counting the number of unique words.
-        # TODO: find out average vocabulary size of a credible article if exists
 
         for word in words:
             word = re.sub('[.,!?(){}<>]', '', word)
@@ -78,12 +81,13 @@ def formality_check():
             if words[i] != '':
                 if (words[i])[0].islower() or i==0:
                     if words[i] != spell(words[i]):
-                        spelling_mistakes_count += 1
+                        if words[i] not in incorrect_words:
+                            incorrect_words.append(words[i])
                         # print(words[i])
                 elif (words[i])[0].isupper() and '.' in words[i-1] or '?' in words[i-1] or '!' in words[i-1]:
                     if words[i] != spell(words[i]):
-                        spelling_mistakes_count += 1
-                        # print(words[i])
+                        if words[i] not in incorrect_words:
+                            incorrect_words.append(words[i])
 
     title_words = article_paragraphs[0].split(' ')
 
@@ -98,41 +102,50 @@ def formality_check():
             title_spelling_mistakes_count += 1
             # print(title_words[i])
 
-    spelling_error_rate = (spelling_mistakes_count/total_word_count)*100
-    lexical_richness = len(unique_words)/total_word_count
+    formality_results['num_spelling_errors'] = len(incorrect_words)
+    formality_results['num_spelling_errors_title'] = title_spelling_mistakes_count
+    formality_results['num_consecutive_marks'] = consecutive_marks_count
+    formality_results['num_marks_title'] = marks_count_title
+    formality_results['num_marks_text'] = marks_count - marks_count_title
+    formality_results['num_all_caps'] = all_caps_words_count
+    formality_results['lexical_richness'] = len(unique_words)/total_word_count
+    formality_results['misspelled_words'] = incorrect_words
+    formality_results['capitalized_words'] = capitalized_words
 
     # Clear the file if there's already something written in it
-    open("formalityTestResults.txt", "w").close()
+    #open("formalityTestResults.txt", "w").close()
 
-    formality_test_results = open("formalityTestResults.txt", "a")
+    #formality_test_results = open("formalityTestResults.txt", "a")
 
     # Copy the results of our formality check to the results file -> we will use it in the test cases
-    line1_result = formality_test_results.write(str(marks_count_title))
-    line1_message = formality_test_results.write(' ! or ? in title\n')
+    #line1_result = formality_test_results.write(str(marks_count_title))
+    #line1_message = formality_test_results.write(' ! or ? in title\n')
 
-    line2_result = formality_test_results.write(str(marks_count))
-    line2_message = formality_test_results.write(' ? in total\n')
+    #line2_result = formality_test_results.write(str(marks_count))
+    #line2_message = formality_test_results.write(' ? in total\n')
 
-    line3_result = formality_test_results.write(str(consecutive_marks_count))
-    line3_message = formality_test_results.write(' consecutive ! or ? \n')
+    #line3_result = formality_test_results.write(str(consecutive_marks_count))
+    #line3_message = formality_test_results.write(' consecutive ! or ? \n')
 
-    line4_result = formality_test_results.write(str(all_caps_words_count))
-    line4_message = formality_test_results.write( ' words in all caps found \n')
+    #line4_result = formality_test_results.write(str(all_caps_words_count))
+    #line4_message = formality_test_results.write( ' words in all caps found \n')
 
-    line5_result = formality_test_results.write(str(title_spelling_mistakes_count))
-    line5_message = formality_test_results.write(' spelling mistakes found in title \n')
+    #line5_result = formality_test_results.write(str(title_spelling_mistakes_count))
+    #line5_message = formality_test_results.write(' spelling mistakes found in title \n')
 
-    if spelling_error_rate >= 1:
-        line6 = formality_test_results.write('Unacceptable error rate \n')
-    else:
-        line6 = formality_test_results.write('Acceptable error rate \n')
+    #if spelling_error_rate >= 1:
+    #    line6 = formality_test_results.write('Unacceptable error rate \n')
+    #else:
+    #    line6 = formality_test_results.write('Acceptable error rate \n')
 
-    if lexical_richness > 0.4:
-        line7 = formality_test_results.write('Higher than average lexical density')
-    else:
-        line7 = formality_test_results.write('Average or less than average lexical richness')
+    #if lexical_richness > 0.4:
+    #    line7 = formality_test_results.write('Higher than average lexical density')
+    #else:
+    #    line7 = formality_test_results.write('Average or less than average lexical richness')
 
-    formality_test_results.close()
+    #formality_test_results.close()
+
+    return formality_results
 
 #formality_check()
 
